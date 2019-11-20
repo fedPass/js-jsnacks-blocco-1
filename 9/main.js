1a1b09f658007f376a5ab22e5a7ee68e1096095f
fedPass <federica.passanante@gmail.com>
2019-11-20 19:45:49 +0100

    es 9

---
+++ 9/main.js
@@ -0,0 +1,8 @@
+// Calcola la somma e la media dei primi 10 numeri.
+var somma = 0;
+for (var i = 1; i <= 10; i++) {
+    somma = (somma) + i;
+}
+var media = somma / 10;
+console.log('la somma è ' + somma);
+console.log('la media è ' + media);
