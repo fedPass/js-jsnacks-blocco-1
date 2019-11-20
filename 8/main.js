59f86c9f7c69c4f2e68a7d4247864cf46edaf317
fedPass <federica.passanante@gmail.com>
2019-11-20 19:18:52 +0100

    num 8 finito

--- 8/main.js
+++ 8/main.js
@@ -2,32 +2,13 @@
 // calcola la somma di tutte le cifre che compongono il numero.
 
 var numero = prompt('Inserisci un numero di 4 cifre');
-console.log('hai inserito ' + numero);
-numeroStr = numero.toString();
-var numeri = [];
-for (var i = 0; i < numeroStr.lenght; i++) {
-    numeri.push(numeroStr.charAt(i))
-}
-console.log(numeri);
-
-var somma = 0;
-for (var i = 0; i < numeri.length; i++) {
-    somma = somma + numeri[i];
-}
-
-console.log('la somma è ' + somma);
-
+console.log(numero);
 // if (numero.lenght != 4) {
-//     console.log('Non hai inserito un numero da 4 cifre');
+//     console.log('Hai inserito un numero che non ha 4 cifre')
 // } else {
-//     console.log(numero);
-// }
-
-// if (numero.lenght == 4) {
-//     var somma = 0;
-//     for (var i = 0; i < 4; i++) {
-//         somma = somma + numero[i]
-//     }
-// } else {
-//     console.log('Non hai inserito un numero da 4 cifre');
+    var somma = 0;
+    for (var i = 0; i < numero.length; i++) {
+        somma = (somma) + parseInt(numero[i]);
+    }
+    console.log('la somma è ' + somma);
 // }
